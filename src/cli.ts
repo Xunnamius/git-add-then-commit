3efa204589a53a32f5ccaace26560693dc0a7c45
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2021-11-08 15:01:00 -0800

    refactor(src): slightly more helpful top-level error handling

--- src/cli.ts
+++ src/cli.ts
@@ -6,11 +6,13 @@ const debug = debugFactory(`${pkgName}:cli`);
 
 export default (({ program, parse }) =>
   parse().catch(async (e: Error | string) => {
-    !(await program.argv).silent &&
+    if (!(await program.argv).silent) {
       // eslint-disable-next-line no-console
       console.error(
         `fatal: ${(typeof e == 'string' ? e : e.message).replace(/^fatal: /, '')}`
       );
-    debug(`error handler: ${e}`);
+    }
+
+    debug.extend('<error>')('%O', e);
     process.exit(1);
   }))(configureProgram());
